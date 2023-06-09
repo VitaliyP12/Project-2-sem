ac4f94a9bf0980faf0d37d9e6e5537ff584bdef6
Perchuk Vitaliy <v.perchuk04@gmail.com>
2023-06-09 23:16:52 +0300

    Functions: matrix multiplication by scalar,multiplication of two matrices,transpose

--- project.js
+++ project.js
@@ -60,3 +60,58 @@ function createMatrix(rows, cols) {
   
     return resultMatrix;
   }
+
+  // Function to multiply a matrix by a scalar
+  function multiplyByScalar(matrix, scalar) {
+    const rows = matrix.length;
+    const cols = matrix[0].length;
+  
+    const resultMatrix = createMatrix(rows, cols);
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < cols; j++) {
+        resultMatrix[i][j] = matrix[i][j] * scalar;
+      }
+    }
+  
+    return resultMatrix;
+  }
+  
+  // Function to multiply two matrices
+  function multiplyMatrices(matrixA, matrixB) {
+    const rowsA = matrixA.length;
+    const colsA = matrixA[0].length;
+    const rowsB = matrixB.length;
+    const colsB = matrixB[0].length;
+  
+    if (colsA !== rowsB) {
+      return null;
+    }
+  
+    const resultMatrix = createMatrix(rowsA, colsB);
+    for (let i = 0; i < rowsA; i++) {
+      for (let j = 0; j < colsB; j++) {
+        let sum = 0;
+        for (let k = 0; k < colsA; k++) {
+          sum += matrixA[i][k] * matrixB[k][j];
+        }
+        resultMatrix[i][j] = sum;
+      }
+    }
+  
+    return resultMatrix;
+  }
+  
+  // Function to transpose a matrix
+  function transposeMatrix(matrix) {
+    const rows = matrix.length;
+    const cols = matrix[0].length;
+  
+    const resultMatrix = createMatrix(cols, rows);
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < cols; j++) {
+        resultMatrix[j][i] = matrix[i][j];
+      }
+    }
+  
+    return resultMatrix;
+  }
